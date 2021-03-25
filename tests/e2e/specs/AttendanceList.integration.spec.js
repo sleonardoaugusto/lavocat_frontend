e357ff1aa22ead4f111ac11ec792074a4a59b762
Leonardo Augusto <sleonardoaugusto@gmail.com>
2021-03-25 03:03:36 -0300

    WIP: Form fill tests

--- tests/e2e/specs/AttendanceList.integration.spec.js
+++ tests/e2e/specs/AttendanceList.integration.spec.js
@@ -1,21 +1,24 @@
 import attendances from '../fixtures/attendances/attendances.json'
 
 describe('<AttendanceList />', () => {
-  before(() => {
-    const baseUrl = Cypress.env('host')
-    const apiServer = Cypress.env('api_server')
+  const baseUrl = Cypress.env('host')
+  const apiServer = Cypress.env('api_server')
 
-    cy.visit(`${baseUrl}/atendimentos`)
-
+  beforeEach(() => {
     cy.server()
     cy.route('GET', `${apiServer}/attendances/`, attendances).as('attendances')
   })
 
   it('Must render attendance list page', () => {
+    cy.visit(`${baseUrl}/atendimentos`)
+
     cy.contains('Lista de Atendimentos')
   })
 
   it('Must go to attendance page', () => {
+    cy.visit(`${baseUrl}/atendimentos`)
+    cy.wait('@attendances')
+
     cy.get('#attendance-1').click()
 
     cy.url().should('include', '/atendimentos/1/editar')
--- tests/e2e/specs/AttendanceUpdate.integration.spec.js
+++ tests/e2e/specs/AttendanceUpdate.integration.spec.js
@@ -1,15 +1,19 @@
-import createAttendance from '../fixtures/attendances/create.json'
 import statuses from '../fixtures/attendances/statuses.json'
+import attendances from '../fixtures/attendances/attendances.json'
 
 describe('<AttendanceUpdate />', () => {
   const baseUrl = Cypress.env('host')
   const apiServer = Cypress.env('api_server')
+  const attendanceResp = attendances[0]
 
   beforeEach(() => {
     cy.server()
 
     cy.route('GET', `${apiServer}/attendance-statuses/`, statuses).as(
       'attendanceStatuses'
+    )
+    cy.route('GET', `${apiServer}/attendances/1/`, attendanceResp).as(
+      'attendanceGetById'
     )
   })
 
@@ -19,9 +23,13 @@ describe('<AttendanceUpdate />', () => {
     cy.contains('Editar Atendimento')
   })
 
-  // it('Must fill fields with response content', () => {
-  //   cy.visit(`${baseUrl}/atendimentos/1/editar`)
-  //
-  //   cy.wait('@attendanceStatuses')
-  // })
+  it.todo('Must fill fields with response content', () => {
+    cy.visit(`${baseUrl}/atendimentos/1/editar`)
+
+    cy.wait('@attendanceStatuses')
+    cy.wait('@attendanceGetById')
+
+    cy.get('#customer-name').should('have.value', attendanceResp.customer_name)
+    cy.get('#document-id').should('have.value', attendanceResp.document_id)
+  })
 })
