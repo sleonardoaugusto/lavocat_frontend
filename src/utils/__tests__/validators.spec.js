913e51668f5c19ade2563a1541b37888753bdf79
Leonardo Augusto <sleonardoaugusto@gmail.com>
2021-03-29 19:08:08 -0300

    Tests refactor

--- src/utils/__tests__/validators.spec.js
+++ src/utils/__tests__/validators.spec.js
@@ -1,14 +1,14 @@
 import { cpfValidator } from '@/utils/validators'
 
 describe('validators.js', () => {
-  it('Must remove dots and commas', () => {
-    expect(cpfValidator('999.999.999-99')).toBe(true)
+  it.each([
+    ['999.999.999-99', true],
+    ['999.999.999-9', false],
+    [9999999999, false],
+    [99999999999, true],
+    ['9999999999', false],
+    ['99999999999', true]
+  ])('%s value must return %s', (value, bool) => {
+    expect(cpfValidator(value)).toBe(bool)
   })
-
-  it.each([['999.999.999-9'], [9999999999]])(
-    '%s value must return false',
-    value => {
-      expect(cpfValidator(value)).toBe(false)
-    }
-  )
 })
