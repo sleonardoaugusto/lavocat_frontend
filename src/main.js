1c2d63b97cd0c20554f27d94eb8a6b537a3804b4
leonardoaugusto <sleonardoaugusto@gmail.com>
2022-11-28 23:17:43 -0300

    sentry removed

--- src/main.js
+++ src/main.js
@@ -6,28 +6,11 @@ import Vuelidate from 'vuelidate'
 import busy from '@/mixins/busy'
 import VueTheMask from 'vue-the-mask'
 import services from '@/services'
-import * as Sentry from '@sentry/vue'
-import { BrowserTracing } from '@sentry/tracing'
 
 Vue.config.productionTip = false
 Vue.use(Vuelidate)
 Vue.use(VueTheMask)
 Vue.mixin(busy)
-
-Sentry.init({
-  Vue,
-  dsn: process.env.SENTRY_DSN,
-  integrations: [
-    new BrowserTracing({
-      routingInstrumentation: Sentry.vueRouterInstrumentation(router),
-      tracingOrigins: ['localhost', 'my-site-url.com', /^\//]
-    })
-  ],
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0
-})
 
 new Vue({
   router,
