d3d389a01cfb4d655d0f0f9399bf7412cfb0a403
leonardoaugusto <sleonardoaugusto@gmail.com>
2023-09-04 23:17:51 -0300

    fix service type

--- src/constants.js
+++ src/constants.js
@@ -4,7 +4,7 @@ export const attendanceTypeOptions = [
   { value: 'AUXILIO_ACIDENTE', text: 'Auxílio Acidente' },
   { value: 'LOAS', text: 'LOAS' },
   { value: 'APOSENTADORIA', text: 'Aposentadoria' },
-  { value: 'ACAO_CONTRA_CONDUTOR', text: 'Ação Contra Condutor' },
+  { value: 'ACAO_INDENIZATORIA', text: 'Ação Indenizatória' },
   { value: 'ACAO_TRABALHISTA', text: 'Ação Trabalhista' },
   { value: 'ACAO_PREVIDENCIARIA', text: 'Ação Previdenciária' },
   { value: 'SEGURO_DE_VIDA_PROPRIO', text: 'Seguro de Vida Próprio' },
